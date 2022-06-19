31bee41ed393d1e7f812e72f8991d5faa290124b
dipanshu <medipansh@gmail.com>
2022-06-19 22:44:30 +0530

    git add .

--- String Program/String_01.java
+++ String Program/String_01.java
@@ -1,6 +1,16 @@
 // Java Program to count the total number of characters in a string
 public class String_01 {
     public static void main(String[] args) {
-
+        //creating string variable
+        String str = "The world to come";
+        //counter to count the no of characters
+        int count = 0;
+        // main logic
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) != ' '){
+                count++;
+            }
+        }
+        System.out.println("The number of characters in line '" + str + "' is " + count);
     }
 }
